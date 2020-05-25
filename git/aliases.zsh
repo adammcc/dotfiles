3e828050d8ef9a59e12db8edf6202862df97dab6
adammcc <ajm845@gmail.com>
2020-05-25 12:17:41 -0400

    Adds a few more aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,12 +15,16 @@ alias co='git checkout'
 alias cob="git checkout -b"
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -sb'
 alias master='git checkout master'
 alias gstat='git status'
 alias gpo='git push origin'
 alias gpom='git push origin master'
+alias gpull='git pull origin master'
+alias grbm='git rebase master'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gl1='gl -1'
 alias gl3='gl -3'
 alias gl5='gl -5'
+alias gl10='gl -10'
+alias editcreds='bin/rails credentials:edit'
--- subl/subl.zsh
+++ subl/subl.zsh
@@ -1,1 +1,3 @@
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias getnotes="subl /Users/adammccabe/Code/xpand_notes.txt"
+alias getscripts="subl /Users/adammccabe/Code/scripts.rb"
